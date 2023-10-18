36f91e17625c43131a2d60e3d94594ba62ed4a5d
shohaib1996 <khanshohaibhossain@gmail.com>
2023-10-18 10:42:28 +0600

    Product collection created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express()
 const port = process.env.PORT || 5000;
@@ -9,8 +9,8 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-app.get("/", (req, res)=> {
-    res.send("Hi Hello there")
+app.get("/", (req, res) => {
+  res.send("Hi Hello there")
 })
 // autoCareCategory
 // GD3XDodlQuEvCsQg
@@ -32,19 +32,28 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const database = client.db("brandDB");
-    const brandCollection = database.collection("brands");
 
-    app.post("/brands", async(req, res)=> {
-        const brads = req.body;
-        const result = await brandCollection.insertOne(brads)
-        res.send(result)
+    const brandCollection = client.db("brandDB").collection("brands");
+    const productCollection = client.db("brandDB").collection("products")
+
+    app.post("/brands", async (req, res) => {
+      const brads = req.body;
+      const result = await brandCollection.insertOne(brads)
+      res.send(result)
     })
-    app.get("/brands", async(req, res) => {
-        const cursor = brandCollection.find()
-        const result = await cursor.toArray()
-        res.send(result)
+    app.get("/brands", async (req, res) => {
+      const cursor = brandCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
     })
+    app.get("/brands/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await brandCollection.findOne(query)
+      res.send(result)
+    })
+
+    // Product related apis
 
 
 
@@ -60,6 +69,6 @@ async function run() {
 run().catch(console.dir);
 
 
-app.listen(port, ()=> {
-    console.log(`Hello world is running on ${port}`);
+app.listen(port, () => {
+  console.log(`Hello world is running on ${port}`);
 })
