22f5acefe7315771d2697b88b6e97dd77b76849c
shohaib1996 <khanshohaibhossain@gmail.com>
2023-10-21 01:21:49 +0600

    cart delete funcrionality added

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ async function run() {
     })
     app.delete("/cart/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: id}
+      const query = { _id: new ObjectId(id)}
       const result = await cartCollection.deleteOne(query)
       console.log(id);
       res.send(result)
